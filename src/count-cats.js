462df388c765f15e0110a715ea751bef1607dd10
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-12-15 00:56:02 +0100

    feat: countCats completed

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let catNum = 0;
+  const catEars = '^^';
+  matrix.forEach((item) => {
+    let catHome = [];
+    catHome = item.filter((isCat) => {
+      return isCat === catEars;
+    });
+    catNum += catHome.length;
+  });
+  return catNum;
 }
 
 module.exports = {
