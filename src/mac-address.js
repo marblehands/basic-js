8b44eb84b4986bb6933a81830d5742d646800a66
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-12-16 15:57:07 +0100

    feat: Mac Address task is done

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  let arr = n.split('-').join('').split('');
+  console.log(arr)
+  let alphabet = '0123456789ABCDEF';
+  let alphabetArr = alphabet.split('');
+  for (let num of arr) {
+    if (!alphabetArr.includes(num)) {
+      return false;
+    }
+  }
+  return true;
 }
 module.exports = {
   isMAC48Address
