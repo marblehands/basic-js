d316af46108ada74d248c183f2f8ffdc76ac51a0
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-12-16 21:19:22 +0100

    feat: Carbon dating task is done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
+
+  if (isNaN(Number(sampleActivity)) || typeof sampleActivity !== 'string' || Number(sampleActivity) >= 15 || Number(sampleActivity) <= 0 || arguments.length === 0) {
+    return false;
+  }
+
+  const result = Math.ceil((Math.log(MODERN_ACTIVITY / Number(sampleActivity)) * HALF_LIFE_PERIOD) / Math.log(2));
+
+  return result;
 }
 
 module.exports = {
